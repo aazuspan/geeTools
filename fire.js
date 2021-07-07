b6b4aa052f893b457716f6a335373ac0ab853f5b
aa.zuspan <aa.zuspan@gmail.com>
2021-07-07 16:53:25 +0000

    Updated fire.js (added 1 line, deleted 1 line).

--- fire.js
+++ fire.js
@@ -293,7 +293,7 @@ exports.vectorizeBoundary = function (
   }
   // Convert the FeatureCollection to a Feature
   poly = ee.Feature(poly.geometry());
-  poly = poly.set({ date: img.get("end_date") });
+  poly = poly.set({ start_date: img.get("start_date"), end_date: img.get("end_date") });
   return poly;
 };
 
